f47b3277bb151206c61e11014907514b57fde65c
SupunSS <93522604+SupunSS@users.noreply.github.com>
2024-03-29 16:27:27 +0530

    updated code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './todo/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle("swagger test")
+  .setDescription("this is the test in swagger")
+  .setVersion("1.0.7")
+  .build();
+
+  const document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api',app,document);
   await app.listen(3000);
 }
 bootstrap();
